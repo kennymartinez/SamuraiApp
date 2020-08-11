8246a7a06f3f3c11dfdecbb27884ea08dc56d5d9
Kenny Martinez <kenny.martinez@pm.me>
2020-08-10 19:12:08 -0500

    Adding a web api to implement EF core

--- Samurai.Data/SamuraiContext.cs
+++ Samurai.Data/SamuraiContext.cs
@@ -1,36 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using Samurai.Domain;
 
 namespace Samurai.Data
 {
     public class SamuraiContext : DbContext
     {
-        public DbSet<Domain.Samurai> Samurais { get; set; }
-        public DbSet<Clan> Clans { get; set; }
-        public DbSet<Quote> Quotes { get; set; }
-
-        public static readonly ILoggerFactory ConsoleLoggerFactory =
-            LoggerFactory.Create(builder =>
-            {
-                builder
-                    .AddFilter((category, level) => category == DbLoggerCategory.Database.Command.Name
-                        && level == LogLevel.Information)
-                    .AddConsole();
-            });
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public SamuraiContext(DbContextOptions<SamuraiContext> options)
+            : base(options)
         {
-            optionsBuilder
-                .UseLoggerFactory(ConsoleLoggerFactory)
-                .UseSqlServer(
-                    "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
         }
+
+        public DbSet<Samurai.Domain.Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        // public DbSet<SamuraiBattleStat> SamuraiBattleStats { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
             modelBuilder.Entity<Horse>().ToTable("Horses");
+            // modelBuilder.Entity<SamuraiBattleStat>().HasNoKey().ToView("SamuraiBattleStats");
         }
     }
 }
